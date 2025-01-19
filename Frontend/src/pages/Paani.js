4c989ea2d43999e88ec0d09d94a44649ad258a50
Irtiza1 <irtiza.s2918@gmail.com>
2025-01-19 13:23:27 +0500

    updated

--- Frontend/src/pages/Paani.js
+++ Frontend/src/pages/Paani.js
@@ -104,7 +104,7 @@ export default function DashboardAppPage() {
   return (
     <>
       <Helmet>
-        <title> Dashboard | Minimal UI </title>
+      <title> Paani Dashboard | Sahulat.io </title>
       </Helmet>
 
       <Container maxWidth="xl">
